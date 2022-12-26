71e1a52a91fa77ec0c79fbbcfb88eee2db9f90d4
BiswasAmitav <biswasamit2648@gmail.com>
2022-12-26 11:34:57 +0530

    some practice programe is added

---
+++ Book_code/loop.py
@@ -0,0 +1,36 @@
+# for i in range(10):
+#     print("I want to be a great programmer . . . ")
+
+
+# for i in range(5):
+#     print(i)
+
+# result = 0
+# for i in range(50):
+#     result +=1
+# print(result)
+
+# result = 0;
+# num = 1;
+# for i in range(50):
+#     result +=num;
+#     num = num+1;
+# print(result);
+
+
+# result = 0
+# for num in range(51):
+#     result = result + num
+# print(result)
+
+
+# for i in range(1,20,5):
+#     print(i)
+
+
+result = 0;
+for num in range(101):
+    if num % 5 == 0:
+        print(num)
+        result += num
+print("Sum is : ", result)
---
+++ Book_code/saarc.py
@@ -0,0 +1,14 @@
+number_list=[1,2,3,4,5,6,7,8,9,10];
+print(number_list);
+
+saarc=['Bangladesh','Afghanistan','Bhutan','Nepal','India','Pakistan','Sri lanka'];
+
+country=input("Enter the name of the country : ")
+if country in saarc:
+    print(country, "is a member of SAARC");
+else:
+    print(country, "is not a member of SAARC")
+
+
+print("Program terminated");
+
