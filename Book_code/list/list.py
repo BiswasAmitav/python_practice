a16e88b8f9d96f5f927f48317077e0b18b22b5a9
BiswasAmitav <biswasamit2648@gmail.com>
2022-12-29 12:28:34 +0530

    in this tutorial i learned some od the list method and techniques to operate list

---
+++ Book_code/list/list.py
@@ -0,0 +1,25 @@
+saarc=["Bangladesh","India","sri Lanka","Pakistan","Nepal","Bhutan"]
+
+print(saarc)
+
+saarc.append("Afganistan") #append is used to add element on list .
+print(saarc)
+
+
+saarc.sort()# It sorts the element according to charecter precedence..
+print(saarc);
+
+li=[1,2,4,53,3,5,7,75,4,3]
+li.sort() #it sorts the element based on their data types..
+print(li)
+
+li.reverse() #it reverse the element..
+print(li)
+
+
+fruits=["mango","banana","orange","jackfruits"]
+fruits.reverse()
+print(fruits)
+fruits.insert(0,"Plam")
+print(fruits)
+
